18e9cc881b3f3889776adf490899e2daab9a6509
selfenergy <niklas.gergs@gmx.de>
2021-05-01 06:17:53 +0200

    Add tlsa record generation script

---
+++ files_templates/tlsa.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+printf '_443._tcp.%s. IN TLSA 1 1 1 %s\n' ${domain} $(openssl x509 -in /etc/letsencrypt/live/${domain}/cert.pem -noout -pubkey |
+        openssl pkey -pubin -outform DER |
+        openssl dgst -sha256 -binary |
+        hexdump -ve '/1 "%02x"')
